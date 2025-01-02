0085ac6a0c1c7396b6480aa490f3636919c292a7
harshxsingh14 <147037629+harshxsingh14@users.noreply.github.com>
2025-01-02 16:45:19 +0530

    Delete Cohort 3.0/Week #2 directory

--- Cohort 3.0/Week #2/index1.js
+++
@@ -1,9 +0,0 @@
- //synchrous code
- 
- const read =  require("fs");
-
- const contents  = read.readFile("a.txt", "utf-8"); //Asynchronsoly
- console.log(contents)
-
- const contents2 = read.readFileSync("b.txt","utf-8");//synchrously
-
