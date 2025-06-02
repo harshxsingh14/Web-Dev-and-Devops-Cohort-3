f35d03c3dcbaf2a72c01051d76c4fe74a2625d73
harshxsingh14 <harsh.singh1403@gmail.com>
2025-06-02 18:49:11 +0530

    Create index.js

---
+++ Week #5/5.1 - Headers, Query params and Express/Lecture Codes/03_Create an HTTP Server/index.js
@@ -0,0 +1,46 @@
+
+
+const express = require("express");
+
+const app = express();
+
+app.get("/sum", function (req, res) {
+
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a + b,
+    });
+});
+
+app.get("/subtract", function (req, res) {
+
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a - b,
+    });
+});
+
+app.get("/multiply", function (req, res) {
+
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a * b,
+    });
+});
+
+app.get("/divide", function (req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a / b,
+    });
+});
+
+app.listen(3000);
