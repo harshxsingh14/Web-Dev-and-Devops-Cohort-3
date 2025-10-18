3ebca69c8e4708ea6c53be2d4125a532cc622380
harshxsingh14 <harsh.singh1403@gmail.com>
2025-10-18 20:39:32 +0530

    Next js

---
+++ Week #19/Next_js/01_next_app/app/page.tsx
@@ -0,0 +1,24 @@
+export default async function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1 style={{ fontFamily: "Arial, sans-serif", color: "#2c3e50" }}>
+        Welcome to the Fetch Demonstration Page! 🌟
+      </h1>
+      <p style={{ fontSize: "18px", color: "#34495e" }}>
+        This page serves as an introduction to fetching data with Server-Side Rendering (SSR).
+      </p>
+      <h2 style={{ fontWeight: "normal", marginTop: "30px", color: "#16a085" }}>
+        To see the fetched result, kindly navigate to: 
+        <a 
+          href="http://localhost:3000/user" 
+          style={{ textDecoration: "none", color: "#2980b9" }}
+        >
+          localhost:3000/user
+        </a>
+      </h2>
+      <p style={{ marginTop: "20px", fontSize: "16px", color: "#7f8c8d" }}>
+        Happy coding! 🚀
+      </p>
+    </div>
+  );
+}
