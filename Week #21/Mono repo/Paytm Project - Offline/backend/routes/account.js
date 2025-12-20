6369913959cf3d17b46d9821b9f8448b79c45a2e
HARSH KUMAR SINGH <147037629+harshxsingh14@users.noreply.github.com>
2025-12-20 23:12:16 +0530

    Update account.js

--- Week #21/Mono repo/Paytm Project - Offline/backend/routes/account.js
+++ Week #21/Mono repo/Paytm Project - Offline/backend/routes/account.js
@@ -41,7 +41,6 @@ router.post("/transfer", authMiddleware, async (req, res) => {
         });
     }
 
-    // Perform the transfer
     await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
     await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
 
